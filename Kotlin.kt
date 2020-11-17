95501a143fdae2542762b01bdc265ff0c847d878
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 15:03:40 +0600

    Update and rename Kotlin to Kotlin.kt

--- Kotlin.kt
+++ Kotlin.kt
@@ -1,8 +1,8 @@
 import java.util.Scanner
 fun main(args: Array<String>) {
-   val read = Scanner(System.`in`)
+    val read = Scanner(System.`in`)
     var num1 = read.nextInt()
     var num2 = read.nextInt()
 
-    println("PROD = "+(num1*num2))
+    println("PROD = " + (num1 * num2))
 }
