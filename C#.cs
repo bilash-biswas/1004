9255456192dc1b857dfef6f4151f8281faa1aff4
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 15:01:04 +0600

    Update and rename C# to C#.cs

--- C#.cs
+++ C#.cs
@@ -9,7 +9,7 @@ namespace Sum
             int a, b;
             a = Convert.ToInt32(Console.ReadLine());
             b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("PROD = " + (a*b));
+            Console.WriteLine("PROD = " + (a * b));
             Console.ReadLine();
         }
     }
