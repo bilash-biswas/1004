5f037b24e1f89204227b1bb83e322dcea36e0ca1
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 15:02:32 +0600

    Update and rename Haskell to Haskell.hs

--- Haskell.hs
+++ Haskell.hs
@@ -2,4 +2,4 @@ main :: IO ()
 main = do
         a <- readLn :: IO Int
         b <- readLn :: IO Int
-        putStrLn $ id ("PROD = " ++ show(a*b))
+        putStrLn $ id ("PROD = " ++ show(a * b))
