d0f39663723fa36a1ba7f19ecf773f735391d7c3
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 15:03:06 +0600

    Update and rename Java to Java.java

--- Java.java
+++ Java.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
      int a,b;
      Scanner input = new Scanner(System.in);
      a = input.nextInt();
      b = input.nextInt();
-        System.out.printf("PROD = %d\n",a*b);
+     System.out.printf("PROD = %d\n", a * b);
     }
-    
 }
